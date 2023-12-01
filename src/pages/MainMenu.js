ded46ad4670a7229ee48a029338d3bf121a90cc2
Abdul Mateen <abdulmateen4890@gmail.com>
2023-12-01 13:06:52 +0500

    swipper added

--- src/pages/MainMenu.js
+++ src/pages/MainMenu.js
@@ -1,38 +1,46 @@
-import '../renderer/App.css';
 import { NavLink } from 'react-router-dom';
+import React, { useEffect, useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { Mousewheel, Pagination } from 'swiper/modules';
+import '../renderer/App.css';
+import 'swiper/css';
 import mainMenu from './TSM-img/main_menu.svg';
+
+SwiperCore.use([Mousewheel, Pagination]);
 
 export default function MainMenu() {
   const buttonArray = [
-    {
-      name: 'TEST MODE',
-      link: '/',
-    },
-    {
-      name: 'TUTORIALS',
-      link: '/',
-    },
-    {
-      name: 'START SIMULATION',
-      link: '/',
-    },
-    {
-      name: 'ADD INTSRUCTORS ',
-      link: '/',
-    },
-    {
-      name: 'ADD STUDENTS ',
-      link: '/',
-    },
-    {
-      name: 'PAST SIMULATIONS',
-      link: '/',
-    },
+    { name: 'TEST MODE', link: '/' },
+    { name: 'TUTORIALS', link: '/' },
+    { name: 'START SIMULATION', link: '/' },
+    { name: 'ADD INSTRUCTORS', link: '/' },
+    { name: 'ADD STUDENTS', link: '/' },
+    { name: 'PAST SIMULATIONS', link: '/' },
   ];
+
+  const swiperRef = useRef(null);
+
+  useEffect(() => {
+    const swiperInstance = swiperRef.current.swiper;
+
+    if (swiperInstance) {
+      const scaleSlides = () => {
+        swiperInstance.slides.forEach((slide, index) => {
+          let scale = 1 - Math.abs(index - swiperInstance.activeIndex) * 0.1;
+          scale = Math.max(scale, 0.7);
+          slide.style.transform = `scale(${scale})`;
+        });
+      };
+
+      scaleSlides();
+      swiperInstance.on('slideChange', scaleSlides);
+    }
+  }, []);
 
   return (
     <div
-      className=" mainMenu_main_class"
+      className="mainMenu_main_class"
       style={{ backgroundImage: `url(${mainMenu})` }}
     >
       <div className="main_content">
@@ -46,13 +54,24 @@ export default function MainMenu() {
         <div className="content_second_half">
           <NavLink to="/">Home</NavLink>
           <div className="button_slider">
-            {buttonArray.map((data) => {
-              return (
-                <NavLink to={data.link} className="button_slider_button">
-                  <span>{data.name}</span>
-                </NavLink>
-              );
-            })}
+            <Swiper
+              ref={swiperRef}
+              slidesPerView={7}
+              direction="vertical"
+              centeredSlides={true}
+              loop={true}
+              mousewheel={true}
+              pagination={{ clickable: true }}
+              className="mySwiper"
+            >
+              {buttonArray.map((data, index) => (
+                <SwiperSlide key={index} className="swiper_slide">
+                  <NavLink to={data.link} className="button_slider_button">
+                    <span>{data.name}</span>
+                  </NavLink>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
         </div>
       </div>
